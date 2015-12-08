5d30b8857fef63914d40adfa1299c811f858210e
Alex <alexis1784@gmail.com>
2015-12-08 09:13:19 +0500

    перед основами контроллеров http://metanit.com/sharp/mvc5/3.1.php

---
+++ BookStore3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookStore3.Startup))]
+namespace BookStore3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
